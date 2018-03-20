5261efe2a8a0c811a414c3d8ba486360d4243de5
Denys N <denys@joomla-ua.org>
2018-03-20 23:32:42 +0200

    Update to 2.7.8

--- julib/image.php
+++ julib/image.php
@@ -24,127 +24,161 @@ class JUImg
 	 * @param array $attr
 	 *
 	 * @return string
+	 *
+	 * @since  2.0
 	 */
-	public function Render($url, $attr = array())
-    {
-        if($url !== 'cover')
-        {
-            $url = trim($url, '/');
-            $url = trim($url);
-            $url = rawurldecode($url);
+	public function Render($url, array $attr = array())
+	{
+		if($url !== 'cover')
+		{
+			$url = trim($url, '/');
+			$url = trim($url);
+			$url = rawurldecode($url);
 
-            $_error = 0;
-            if(preg_match('#^(http|https|ftp)://#i', $url))
-            {
-                $headers = @get_headers($url);
-                if(strpos($headers[0], '200') === false)
-                {
-                    $_error = 1;
-                }
-            }
-            else
-            {
-                $url = JPATH_BASE . '/' . $url;
-                if(!file_exists($url))
-                {
-                    $_error = 1;
-                }
-            }
+			$_error = 0;
+			if(preg_match('#^(http|https|ftp)://#i', $url))
+			{
+				$headers = @get_headers($url);
+				if(strpos($headers[ 0 ], '200') === false)
+				{
+					$_error = 1;
+				}
+			}
+			else
+			{
+				$url = JPATH_BASE . '/' . $url;
+				if(!file_exists($url))
+				{
+					$_error = 1;
+				}
+			}
 
-            $imgfile  = pathinfo($url);
-            $img_name = $imgfile['filename'];
+			$imgfile  = pathinfo($url);
+			$img_name = $imgfile[ 'filename' ];
+			$imgurl   = strtolower($img_name);
+			$imgurl   = preg_replace('#[[:punct:]]#', '', $imgurl);
+			$imgurl   = preg_replace('#[а-яёєїіА-ЯЁЄЇІ]#isu', '', $imgurl);
+			$imgurl   = str_replace(array(
+				' +',
+				' '
+			), array(
+				'_',
+				''
+			), $imgurl);
+		}
+		else
+		{
+			$_error   = 0;
+			$img_name = implode($attr);
+			$imgurl   = 'cover';
+		}
 
-            $imgurl = strtolower($img_name);
-            $imgurl = preg_replace('#[[:punct:]]#', '', $imgurl);
-            $imgurl = preg_replace('#[а-яА-Я]#isu', '', $imgurl);
-	        $imgurl = str_replace(array(
-		        ' +',
-		        ' '
-	        ), array(
-		        '_',
-		        ''
-	        ), $imgurl);
-        }
-        else
-        {
-            $_error   = 0;
-            $img_name = implode($attr);
-            $imgurl   = 'cover';
-        }
+		$fext        = array();
+		$wh          = array();
+		$img_cache   = array();
+		$error_image = array();
 
-        $fext        = array();
-        $wh          = array();
-        $img_cache   = array();
-        $error_image = array();
+		if(!empty($attr) && is_array($attr))
+		{
+			foreach($attr as $whk => $whv)
+			{
+				if($whk === 'f')
+				{
+					$fext[] = $whv;
+				}
 
-	    if(!empty($attr) && is_array($attr))
-	    {
-		    foreach ($attr as $whk => $whv)
-		    {
-		        if($whk === 'f')
-		        {
-		            $fext[] = $whv;
-		        }
+				if($whk === 'w' || $whk === 'h')
+				{
+					$wh[] = $whv;
+				}
 
-		        if($whk === 'w' || $whk === 'h')
-		        {
-		            $wh[] = $whv;
-		        }
+				if($whk === 'cache')
+				{
+					$img_cache[] = $whv;
+				}
 
-		        if($whk === 'cache')
-		        {
-		            $img_cache[] = $whv;
-		        }
+				if($whk === 'error_image')
+				{
+					$error_image[] = $whv;
+				}
+			}
+		}
 
-		        if($whk === 'error_image')
-		        {
-		            $error_image[] = $whv;
-		        }
-		    }
-	    }
+		$fext      = implode($fext);
+		$fext      = '.' . ($fext == '' ? 'jpg' : $fext);
+		$img_cache = implode($img_cache);
+		$img_cache = ($img_cache == '' ? 'cache' : $img_cache);
 
-        $fext      = implode($fext);
-        $fext      = '.' . ($fext == '' ? 'jpg' : $fext);
-        $img_cache = implode($img_cache);
-        $img_cache = ($img_cache == '' ? 'cache' : $img_cache);
+		if($_error === '1')
+		{
+			$error_image = implode($error_image);
+			$url         = ($error_image == '' ? JPATH_BASE . '/libraries/julib/noimage.png' : $error_image);
+		}
 
-        if($_error === '1')
-        {
-            $error_image = implode($error_image);
-            $url         = ($error_image == '' ? JPATH_BASE . '/libraries/julib/noimage.png' : $error_image);
-        }
+		$wh        = implode('x', $wh);
+		$wh        = ($wh == '' ? '0' : $wh);
+		$subfolder = $img_cache . '/' . $wh . '/' . strtolower(substr(md5($img_name), -1));
 
-        $wh        = implode('x', $wh);
-        $wh        = ($wh == '' ? '0' : $wh);
-        $subfolder = $img_cache . '/' . $wh . '/' . strtolower(substr(md5($img_name), -1));
+		$md5 = array();
 
-        $md5 = array();
+		if(!empty($attr) && is_array($attr))
+		{
+			foreach($attr as $k => $v)
+			{
+				$f     = explode('_', $k);
+				$k     = $f[ 0 ];
+				$md5[] = $k . $v;
+			}
+		}
 
-	    if(!empty($attr) && is_array($attr))
-	    {
-		    foreach ($attr as $k => $v)
-		    {
-		        $f     = explode('_', $k);
-		        $k     = $f[0];
-		        $md5[] = $k . $v;
-		    }
-	    }
+		$target = $subfolder . '/' . strtolower(substr($imgurl, 0, 150)) . '-' . md5($url . implode('.', $md5)) . $fext;
 
-        $target = $subfolder . '/' . strtolower(substr($imgurl, 0, 150)) . '-' . md5($url . implode('.', $md5)) . $fext;
+		$this->MakeDirectory($dir = JPATH_BASE . '/' . $subfolder, $mode = 0777);
 
-        $this->MakeDirectory($dir = JPATH_BASE . '/' . $subfolder, $mode = 0777);
+		if(file_exists(JPATH_BASE . '/' . $target))
+		{
+			$outpute = $target;
+		}
+		else
+		{
+			$outpute = $this->Create($url, $img_cache, $target, $attr);
+		}
 
-        if(file_exists(JPATH_BASE . '/' . $target))
-        {
-            $outpute = $target;
-        }
-        else
-        {
-            $outpute = $this->Create($url, $img_cache, $target, $attr);
-        }
+		return $outpute;
+	}
 
-        return $outpute;
-    }
+	/**
+	 * @param $dir
+	 * @param $mode
+	 *
+	 * @return bool
+	 *
+	 * @since  2.0
+	 */
+	public function MakeDirectory($dir, $mode)
+	{
+		if(@mkdir($dir, $mode) || is_dir($dir))
+		{
+			$indexfile    = $dir . '/index.html';
+			$indexcontent = '<!DOCTYPE html><title></title>';
+
+			if(!file_exists($indexfile))
+			{
+				$file = fopen($indexfile, 'wb');
+				fwrite($file, $indexcontent);
+				fclose($file);
+			}
+
+			return true;
+		}
+
+		if(!$this->MakeDirectory(dirname($dir), $mode))
+		{
+			return false;
+		}
+
+		return @mkdir($dir, $mode);
+	}
 
 	/**
 	 * @param       $url
@@ -153,117 +187,86 @@ class JUImg
 	 * @param array $attr
 	 *
 	 * @return string
-	 */
-	public function Create($url, $img_cache, $target, $attr = array())
-    {
-        $phpThumb = new JUThumbs();
-
-        $phpThumb->resetObject();
-
-        $phpThumb->setParameter('config_max_source_pixels', round(max((int) ini_get('memory_limit'), (int) get_cfg_var('memory_limit')) * 1048576 / 6));
-        $phpThumb->setParameter('config_temp_directory', JPATH_BASE . '/' . $img_cache . '/');
-        $phpThumb->setParameter('config_cache_directory', JPATH_BASE . '/' . $img_cache . '/');
-
-        $phpThumb->setCacheDirectory();
-
-        $phpThumb->setParameter('config_cache_maxfiles', '0');
-        $phpThumb->setParameter('config_cache_maxsize', '0');
-        $phpThumb->setParameter('config_cache_maxage', '0');
-
-        $phpThumb->setParameter('config_error_bgcolor', 'FAFAFA');
-        $phpThumb->setParameter('config_error_textcolor', '770000');
-
-        $phpThumb->setParameter('config_nohotlink_enabled', false);
-
-        if($url === 'cover')
-        {
-            $cover = array();
-
-	        if(!empty($attr) && is_array($attr))
-	        {
-		        foreach ($attr as $whk => $whv)
-		        {
-		            if($whk === 'cover')
-		            {
-		                $cover[] = $whv;
-		            }
-		        }
-	        }
-
-            $phpThumb->setSourceFilename(JPATH_BASE . '/libraries/julib/blank.png');
-            $phpThumb->setParameter('fltr', 'clr|' . implode($cover));
-        }
-        else
-        {
-            $phpThumb->setSourceFilename($url);
-        }
-
-        $phpThumb->setParameter('q', '80');
-        $phpThumb->setParameter('aoe', '1');
-        $phpThumb->setParameter('f', 'jpg');
-
-        if(is_array($attr))
-        {
-            foreach ($attr as $k => $v)
-            {
-                $f = explode('_', $k);
-                $k = $f[0];
-                $phpThumb->setParameter($k, $v);
-            }
-        }
-
-        $imagemagick = '';
-        if(0 === stripos(PHP_OS, 'WIN'))
-        {
-            $imagemagick = 'C:/ImageMagick/convert.exe';
-        }
-
-        $phpThumb->setParameter('config_imagemagick_path', $imagemagick);
-        $phpThumb->setParameter('config_prefer_imagemagick', true);
-        $phpThumb->setParameter('config_imagemagick_use_thumbnail', true);
-
-        $outpute = '';
-        if($phpThumb->GenerateThumbnail())
-        {
-            if($phpThumb->RenderToFile(JPATH_BASE . '/' . $target))
-            {
-                $outpute = $target;
-            }
-
-            $phpThumb->purgeTempFiles();
-        }
-
-        return $outpute;
-    }
-
-	/**
-	 * @param $dir
-	 * @param $mode
 	 *
-	 * @return bool
+	 * @since  2.0
 	 */
-	public function MakeDirectory($dir, $mode)
-    {
-        if(@mkdir($dir, $mode) || is_dir($dir))
-        {
-            $indexfile    = $dir . '/index.html';
-            $indexcontent = '<!DOCTYPE html><title></title>';
+	public function Create($url, $img_cache, $target, array $attr = array())
+	{
+		$phpThumb = new JUThumbs();
 
-            if(!file_exists($indexfile))
-            {
-                $file = fopen($indexfile, 'wb');
-                fwrite($file, $indexcontent);
-                fclose($file);
-            }
+		$phpThumb->resetObject();
 
-            return true;
-        }
+		$phpThumb->setParameter('config_max_source_pixels', '0');
+		$phpThumb->setParameter('config_temp_directory', JPATH_BASE . '/' . $img_cache . '/');
+		$phpThumb->setParameter('config_cache_directory', JPATH_BASE . '/' . $img_cache . '/');
 
-        if(!$this->MakeDirectory(dirname($dir), $mode))
-        {
-            return false;
-        }
+		$phpThumb->setCacheDirectory();
 
-        return @mkdir($dir, $mode);
-    }
+		$phpThumb->setParameter('config_cache_maxfiles', '0');
+		$phpThumb->setParameter('config_cache_maxsize', '0');
+		$phpThumb->setParameter('config_cache_maxage', '0');
+		$phpThumb->setParameter('config_error_bgcolor', 'FAFAFA');
+		$phpThumb->setParameter('config_error_textcolor', '770000');
+		$phpThumb->setParameter('config_nohotlink_enabled', false);
+
+		if($url === 'cover')
+		{
+			$cover = array();
+
+			if(!empty($attr) && is_array($attr))
+			{
+				foreach($attr as $whk => $whv)
+				{
+					if($whk === 'cover')
+					{
+						$cover[] = $whv;
+					}
+				}
+			}
+
+			$phpThumb->setSourceFilename(JPATH_BASE . '/libraries/julib/blank.png');
+			$phpThumb->setParameter('fltr', 'clr|' . implode($cover));
+		}
+		else
+		{
+			$phpThumb->setSourceFilename($url);
+		}
+
+		$phpThumb->setParameter('q', '80');
+		$phpThumb->setParameter('aoe', '1');
+		$phpThumb->setParameter('f', 'jpg');
+
+		if(is_array($attr))
+		{
+			foreach($attr as $k => $v)
+			{
+				$f = explode('_', $k);
+				$k = $f[ 0 ];
+				$phpThumb->setParameter($k, $v);
+			}
+		}
+
+		$imagemagick = '';
+		if(0 === stripos(PHP_OS, 'WIN'))
+		{
+			$imagemagick = 'C:/ImageMagick/convert.exe';
+		}
+
+		$phpThumb->setParameter('config_imagemagick_path', $imagemagick);
+		$phpThumb->setParameter('config_prefer_imagemagick', true);
+		$phpThumb->setParameter('config_imagemagick_use_thumbnail', true);
+
+		$outpute = '';
+		if($phpThumb->GenerateThumbnail())
+		{
+			if($phpThumb->RenderToFile(JPATH_BASE . '/' . $target))
+			{
+				$outpute = $target;
+			}
+
+			$phpThumb->purgeTempFiles();
+		}
+
+		return $outpute;
+	}
 }
