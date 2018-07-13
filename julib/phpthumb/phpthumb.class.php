f4268e16c9ed1647fdf0c869b049c601c55f56ec
joomla-ua <dgm.denys@gmail.com>
2018-07-14 00:56:31 +0300

    Update to 2.7.10

--- julib/phpthumb/phpthumb.class.php
+++ julib/phpthumb/phpthumb.class.php
@@ -215,7 +215,7 @@ class JUThumbs {
 	public $issafemode       = null;
 	public $php_memory_limit = null;
 
-	public $phpthumb_version = '1.7.15-201806071234';
+	public $phpthumb_version = '1.7.15-201807101405';
 
 	//////////////////////////////////////////////////////////////////////
 
